d5752d63e32f54a21473973b05255f0c9c7b4068
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-03-19 19:21:39 -0500

    Build boilerplate for generating requests

---
+++ NicholasSynovic/program.py
@@ -0,0 +1,29 @@
+from urllib.request import Request, urlopen
+
+class RequestBuilder:
+
+	def __init__(self, token:str):
+		self.url = "https://api.github.com/graphql"
+		self.token = token
+
+	def getURL(self)	->	str:
+		return self.url
+
+	def getToken(self)	->	str:
+		return self.token
+
+	def setToken(self, token:str)	->	None:
+		self.token = token
+
+	def build(self)	->	urllib.request.Request:
+		payload = "{\"query\":\"{\\n    search(query: \\\"language:Python created:<=2018-10-29T00:00:00\\\", type: REPOSITORY, first: 100)    {\\n        edges   {\\n            cursor\\n            node    {\\n                ... on Repository   {\\n                    createdAt\\n                    hasIssuesEnabled\\n                    nameWithOwner\\n                    defaultBranchRef    {\\n                        target  {\\n                            ... on Commit   {\\n                                history(first: 0)   {\\n                                    totalCount\\n                                }\\n                            }\\n                        }\\n                    }\\n                    issues  {\\n                        totalCount\\n                    }\\n                    pullRequests    {\\n                        totalCount\\n                    }\\n                }\\n            }\\n        }\\n    }\\n}\",\"variables\":{}}"
+		
+		headers = {
+			'Authorization': 'token %s' % (self.token),
+			'Content-Type': 'application/json'
+		}
+
+		return Request(url=self.url, data=payload, headers=headers)
+
+b = RequestBuilder(token="544b07c7f24b35ea71422130da45cb3ffc211612")
+print(type(b.build()))
