81eddcece43ed75d4e22f10dd3584c234afe0056
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-03-19 19:32:02 -0500

    Remove explicit token testing

--- NicholasSynovic/program.py
+++ NicholasSynovic/program.py
@@ -1,3 +1,4 @@
+import urllib.request
 from urllib.request import Request, urlopen
 
 class RequestBuilder:
@@ -16,6 +17,7 @@ class RequestBuilder:
 		self.token = token
 
 	def build(self)	->	urllib.request.Request:
+		# Payload generated from request
 		payload = "{\"query\":\"{\\n    search(query: \\\"language:Python created:<=2018-10-29T00:00:00\\\", type: REPOSITORY, first: 100)    {\\n        edges   {\\n            cursor\\n            node    {\\n                ... on Repository   {\\n                    createdAt\\n                    hasIssuesEnabled\\n                    nameWithOwner\\n                    defaultBranchRef    {\\n                        target  {\\n                            ... on Commit   {\\n                                history(first: 0)   {\\n                                    totalCount\\n                                }\\n                            }\\n                        }\\n                    }\\n                    issues  {\\n                        totalCount\\n                    }\\n                    pullRequests    {\\n                        totalCount\\n                    }\\n                }\\n            }\\n        }\\n    }\\n}\",\"variables\":{}}"
 		
 		headers = {
@@ -25,5 +27,8 @@ class RequestBuilder:
 
 		return Request(url=self.url, data=payload, headers=headers)
 
-b = RequestBuilder(token="544b07c7f24b35ea71422130da45cb3ffc211612")
-print(type(b.build()))
+with open("token.txt", "r") as foo:
+	token = foo.read()
+	b = RequestBuilder(token=token)
+	print(b.getToken())
+	print(type(b.build()))
